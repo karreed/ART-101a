1105feb458b03b06cbb5c5b5181065485c186a78
karreed <jonathanau.au21@gmail.com>
2024-04-25 17:02:02 -0700

    java

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,8 +1,17 @@
 // index.js - purpose and description here
-// Author: Your Name
-// Date:
+// Author: Jonathan Au
+// Date: 25 April 2024
 
 // Constants
+
+make = "Toyota";
+model = "Supra";
+color = "Black";
+year = 2020;
+
+// Calculation
+
+age = 2024 - year;
 
 // Functions
 
@@ -15,7 +24,13 @@ function myFunction(param1, param2) {
 function main() {
   console.log("Main function started.");
   // the code that makes everything happen
+  document.writeln("Make: " + make + "<br>");
+  document.writeln("Model: " + model + "<br>");
+  document.writeln("Color: " + color + "<br>");
+  document.writeln("Year: " + year + "<br>");
+  document.writeln("Age: " + age + " years<br>");
 }
 
 // let's get this party started
 main();
+
