296f78f2a630b532266c92747b0b98d9fd552300
karreed <jonathanau.au21@gmail.com>
2024-05-06 20:35:16 -0700

    lab 8

--- lab8/js/lab.js
+++ lab8/js/lab.js
@@ -1,10 +1,36 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Purpose of the program is to make Anon Functions and Callbacks
+// Author: Jonathan Au
+// Date: 6 May 2024
 
 // Constants
+numArray = [2, 5, 6, 8];
 
 // Functions
+function isOdd(x) {
+  return x % 2 != 0;
+}
+
+console.log("is 5 odd? ", isOdd(5));
+console.log("is 8 odd? ", isOdd(8));
+
+console.log("array: ", numArray);
+
+var results = numArray.map(isOdd);
+
+console.log("Results of odd numbers in array: ", results);
+
+var results = numArray.map(function(x) {
+  return x % 2 == 0;
+})
+
+console.log("Results of even numbers in array: ", results);
+
+//map results data
+var mapResults = "map results here: <br />" +"array: " + numArray + "<br /> oddness of array: " + numArray.map(isOdd) + "<br /> evenness of array: " + results;
+
+//Use jQuery to select the element by its ID and set the HTML content
+$("#output").html(mapResults);
+
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
