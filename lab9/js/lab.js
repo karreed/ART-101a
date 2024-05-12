15af92eeaff6c1b658101aa9fc0ce6151b090ae6
karreed <jonathanau.au21@gmail.com>
2024-05-12 16:42:48 -0700

    lab 9

--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -1,10 +1,43 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - The purpose of the lab is to practice using and accesing libraries and jQuery
+// Author: Jonathan Au
+// Date: 7 May 2024
 
 // Constants
 
 // Functions
+
+// add button to challenge & other sections
+$("#challenge").append("<button id='button-challenge'>Make Special</button>");
+
+$("#problems").append("<button id='button-problems'>Make Special</button>");
+
+$("#reflection").append("<button id='button-reflection'>Make Special</button>");
+
+$("#results").append("<button id='button-results'>Make Special</button>");
+
+// $(".minor-section").append("<button id='button-special'>Make Special</button>");
+
+// $("#button-special").click(function(){
+//   $(this).parent().toggleClass("special");
+// });
+
+// add a click listener to the challenge button
+$("#button-challenge").click(function(){
+  // now add (or subtract) the "special" class to the section
+  $("#challenge").toggleClass("special");
+});
+
+$("#button-problems").click(function(){
+  $("#problems").toggleClass("special");
+});
+
+$("#button-reflection").click(function(){
+  $("#reflection").toggleClass("special");
+});
+
+$("#button-results").click(function(){
+  $("#results").toggleClass("special");
+});
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
