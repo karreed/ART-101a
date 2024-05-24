c2b599157d147c56f5c3056e24a0c545ab4bd2f5
karreed <jonathanau.au21@gmail.com>
2024-05-23 18:23:42 -0700

    lab 12

--- lab12/js/lab.js
+++ lab12/js/lab.js
@@ -6,6 +6,39 @@
 
 // Functions
 
+function sortingHat(str) {
+  length = str.length;
+  mod = length % 4;
+  if (mod == 0) {
+    return "Gryffindor"
+  } else if (mod == 1) {
+    return "Ravenclaw";
+  } else if (mod == 2) {
+    return "Slytherin";
+  } else if (mod == 3) {
+    return "Hufflepuff";
+  }
+}
+
+function description(str) {
+  if (str == "Gryffindor") {
+    return "Those in this house are well known for their bravery and courage. Their house animal is the lion and their house colors are scarlet and gold. Gryffindors never back down from a fight or doing what they believe is right.";
+  } else if (str == "Ravenclaw") {
+    return "Prized learning, wisdom, wit, and intellect in its members. Thus, many Ravenclaws tended to be academically motivated and talented students. They also prided themselves on being original in their ideas, and methods.";
+  } else if (str == "Slytherin") {
+    return "Known for being ambitious, cunning, and resourceful. Slytherins are also sometimes regarded as being evil thanks to the fact that many of the most sinister witches and wizards have been associated with this house.";
+  } else if (str == "Hufflepuff") {
+    return "Characteristics include a strong sense of justice, loyalty, patience, and a propensity for hard work. Hufflepuffs are also seen as nice, almost to a fault. Hogwarts houses have moved beyond the realm of fiction and into the real world.";
+  }
+}
+
+$("#button").click(function() {
+  var name = $("#input").val();
+  house = sortingHat(name);
+  $("#output").append("<p>The Sorting Hat has sorted you into " + house + "</p>");
+  $("#output").append("<p>" + description(house) + "</p>");
+})
+
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
   // some code here
