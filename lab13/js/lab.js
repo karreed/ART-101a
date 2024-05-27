687a2cded98247b3a7d21ca29e5852c39b837b6d
karreed <jonathanau.au21@gmail.com>
2024-05-26 20:26:17 -0700

    lab 13

--- lab13/js/lab.js
+++ lab13/js/lab.js
@@ -1,10 +1,31 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - The purpose of the lab is to write a script that uses loops to display common multipliers for numbers. 
+// Author: Jonathan Au
+// Date: 26 May 2024
 
 // Constants
 
 // Functions
+
+function fizzBuzzBoom() {
+  for (var num = 1; num<=200; num++) {
+    var outputStr = "";
+    if (num % 3 == 0) {
+      outputStr += "Fizz";
+    }
+    if (num % 5 == 0) {
+      outputStr += "Buzz";
+    }
+    if (num % 7 == 0) {
+      outputStr += "Boom";
+    }
+    if (outputStr == "") {
+      $("#output").append("<p>" + num + "</p>");
+    } else {
+      $("#output").append("<p>" + outputStr + "</p>");
+    }
+  }
+}
+
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
@@ -19,3 +40,5 @@ function main() {
 
 // let's get this party started
 main();
+
+fizzBuzzBoom();
