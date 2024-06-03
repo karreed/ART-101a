1f0a2973ddc5d80ce867e1b17ee9a9eef3997628
karreed <jonathanau.au21@gmail.com>
2024-06-02 22:17:51 -0700

    lab 15

--- lab15/js/lab.js
+++ lab15/js/lab.js
@@ -1,10 +1,39 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - purpose of the lab is to use AJAX and get data from an url and output in html
+// Author: Jonathan Au
+// Date: 2 June 2024
 
 // Constants
 
 // Functions
+
+$("#activate").click(function() {
+  var pokeId = Math.floor(Math.random() * (1025 - 1 + 1)) + 1;
+  $.ajax({
+    // The URL for the request (from the api docs)
+    url: "https://icanhazdadjoke.com/",
+    // The data to send (will be converted to a query string)
+    // data: { 
+    //         // here is where any data required by the api 
+    //         //   goes (check the api docs)
+    //       },
+    // Whether this is a POST or GET request
+    type: "GET",
+    // The type of data we expect back
+    dataType : "json",
+    // What do we do when the api call is successful
+    //   all the action goes in here
+    success: function(data) {
+        // do stuff
+        const joke = data.joke;
+        $("#output").html(joke);
+    },
+    // What we do if the api call fails
+    error: function (jqXHR, textStatus, errorThrown) { 
+        // do stuff
+        console.log("Error:", textStatus, errorThrown);
+    }
+  })
+})
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
